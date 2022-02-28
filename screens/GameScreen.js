61898fedd8dcc12e07d02d601a75f5eea54fea7a
songhee24 <akaratel61@gmail.com>
2022-02-28 20:18:51 +0600

    Adding Random Number Generation

---
+++ screens/GameScreen.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+import {useState} from "react";
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNum = Math.floor(Math.random() * (max - min) + min);
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+};
+
+const GameScreen = (props) => {
+  const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice))
+
+};
+
+const styles = StyleSheet.create({});
+
+export default GameScreen;
